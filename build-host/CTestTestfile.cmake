6654dd3e52f6c9324a003dc3760ca057ae9e0fd0
Your Name <you@example.com>
2026-08-11 00:29:49 +0330

    Remove build-host artifacts from repo (gitignore build-host/)

--- build-host/CTestTestfile.cmake
+++
@@ -1,9 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /mnt/c/Users/mmd/Dev/AudioRouter
-# Build directory: /mnt/c/Users/mmd/Dev/AudioRouter/build-host
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("src/client")
-subdirs("src/server")
-subdirs("tests")
